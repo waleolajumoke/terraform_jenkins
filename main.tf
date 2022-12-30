da47e9433ceb0739479570f7eef9c565f13729c8
Wale Olajumoke <75453014+waleolajumoke@users.noreply.github.com>
2022-12-30 12:59:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,6 +42,48 @@ resource "aws_security_group" "jenkins_sg" {
   }
 }
 
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = "${aws_iam_role.test_role.name}"
+}
+
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = "${aws_iam_role.test_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+     {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+     }
+  ]
+}
+EOF
+}
 
 resource "aws_instance" "web"{
   ami = var.ami
