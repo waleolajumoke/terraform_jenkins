bafd28f79405842cd4f1683e21fa0793973e91bc
Wale Olajumoke <75453014+waleolajumoke@users.noreply.github.com>
2024-05-30 09:18:42 +0100

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,71 +1,61 @@
 #!/bin/bash
 
 # Install Updated packages on linux machine
-sudo yum update -y
+sudo yum update
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-#sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade -y
 
-#installing java jdk and git
-sudo amazon-linux-extras install java-openjdk11
+#install java, git and nodejs
+sudo dnf install java-17-amazon-corretto-devel -y
 sudo yum install git -y
+sudo yum install nodejs npm -y
 
-#installing maven
+# install maven
 sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 sudo yum install -y apache-maven
 
-#installing jenkins
+# install jenkins and change the port
 sudo yum install jenkins -y
 sudo sed -i -e 's/Environment="JENKINS_PORT=[0-9]\+"/Environment="JENKINS_PORT=8081"/' /usr/lib/systemd/system/jenkins.service
 sudo systemctl daemon-reload
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 
-#installing and unziping AWScli online
+# install awscli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 
-sudo yum install unzip
+sudo yum install unzip -y
 sudo unzip awscliv2.zip  
 sudo ./aws/install
 
-# INstalling ZAP for testing (optional)
-#ZAP is isntalled at /home/ec2-user/ZAP_2.11.1/zap.sh
-sudo wget https://github.com/zaproxy/zaproxy/releases/download/v2.11.1/ZAP_2_11_1_unix.sh
-sudo chmod +x ZAP_2_11_1_unix.sh 
-sudo ./ZAP_2_11_1_unix.sh -q
-sudo tar -xvf ZAP_2.11.1_Linux.tar.gz
+#ZAP is installed and can be run as zap.sh
+sudo wget https://github.com/zaproxy/zaproxy/releases/download/v2.14.0/ZAP_2_14_0_unix.sh
+sudo chmod +x ZAP_2_14_0_unix.sh 
+sudo ./ZAP_2_14_0_unix.sh -q
 
-#installing kubectl
+# install kubectl
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/bin/linux/amd64/kubectl
-
-#changing permission
 chmod +x ./kubectl
-
-#creating directory and copying kubectl to the directory
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 sudo cp kubectl /usr/local/bin/
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
-#installing docker
+# install docker
 sudo yum install docker -y
-
-#creating user and group for docker and jenkins
 sudo usermod -aG docker $USER
 sudo newgrp docker
 sudo usermod -aG docker jenkins
 sudo newgrp docker
 
-#restarting jenkins
+# restart jenkins and docker
 sudo service jenkins restart
 sudo systemctl daemon-reload
-
-#starting and enabling docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker
 
-#json process installation (optional)
+# jq for parsing, transforming, and manipulating JSON data
 sudo yum install jq -y
